7a5ef699fdde10f922e297abe69221c5f1c9a8f8
Daria Hryshchenko <daria199210@gmail.com>
2022-12-22 00:56:22 +0200

    added solution 9 task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtn = document.querySelector('.change-color');
+const infoColorEl = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', (event) => {
+  let currentColor = getRandomHexColor();
+  document.body.style.background = currentColor;
+  infoColorEl.textContent = currentColor;
+})
