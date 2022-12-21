25e7f19eedaeba28b6627cbd8d77e9b15597bc1c
Daria Hryshchenko <daria199210@gmail.com>
2022-12-22 00:35:31 +0200

    added solution 8 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const {
+        elements: {
+            email,
+            password
+        }
+    } = event.currentTarget;
+
+    if (email.value === '' || password.value === '') return alert("Please fill in all the fields!");
+    const dataEl = {
+        email: email.value,
+        password: password.value
+    }
+
+    event.currentTarget.reset();
+
+    console.log(dataEl);
+})
