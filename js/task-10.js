ad6ce522bd472b95c2b90a6d4952add5458d98a1
Daria Hryshchenko <daria199210@gmail.com>
2022-12-23 10:59:27 +0200

    change task 10

--- js/task-10.js
+++ js/task-10.js
@@ -2,17 +2,19 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const inputNumberEl = document.querySelector('#controls>input')
-const createBtnEl = document.querySelector('button[data-create]')
-const destroyBtnEl = document.querySelector('button[data-destroy]')
-const boxesEl = document.querySelector('#boxes')
+const inputNumberEl = document.querySelector('#controls>input');
+const createBtnEl = document.querySelector('button[data-create]');
+const destroyBtnEl = document.querySelector('button[data-destroy]');
+const boxesEl = document.querySelector('#boxes');
+
 
 const createBoxes = event => {
   const addElements = []
   for (let i = 0; i < event; i++) {
+    const firstSize = 30;
     const div = document.createElement('div')
-    div.style.height = `${30 + 10 * i}px`
-    div.style.width = `${30 + 10 * i}px`
+    div.style.height = `${firstSize + i * 10}px`
+    div.style.width = `${firstSize + i * 10}px`
     div.style.background = getRandomHexColor()
     addElements.push(div)
   }
