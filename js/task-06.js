1cacb70d9294301ba6b7315c193abf7d549b6774
Daria Hryshchenko <daria199210@gmail.com>
2022-12-23 10:37:48 +0200

    changed the code in task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,10 +1,13 @@
 const inputTextEl = document.querySelector('#validation-input');
 
-
 inputTextEl.addEventListener('blur', (event) => {
     if (event.currentTarget.value.length >= inputTextEl.getAttribute('data-length')) {
-        inputTextEl.classList.add('valid')
+        inputTextEl.classList.add('valid');
+        inputTextEl.classList.remove('invalid');
+        return;
     } else {
         inputTextEl.classList.add('invalid');
+        inputTextEl.classList.remove('valid');
     }
-})
+
+});
